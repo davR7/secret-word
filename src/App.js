6598ed5472b171374982d064089496d99f77f0bb
Davidson Ratis <davidsonmakarios@gmail.com>
2022-09-17 17:12:45 -0300

    imported from StartScreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
-    <div className="App flex-column-c"></div>
+    <div className="App flex-column-c">
+      <StartScreen />
+    </div>
   );
 }
 
