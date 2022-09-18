5c655c9d95548c3830324e9807a6ed13645b4d33
Davidson Ratis <davidsonmakarios@gmail.com>
2022-09-18 10:33:42 -0300

    Updated StartScreen component - v1.1

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,14 +1,16 @@
+import AppHeading from '../childs/AppHeading';
 import './css/StartScreen.css'
 
 const StartScreen = () => {
     return (
-        <div className="start-screen">
-            <h1 className="start-screen__title">Secret Word</h1>
-            <p className="start-screen__text">
-                Você não pode dizer uma palavra secreta?
-            </p>
+        <main className="start-screen">
+            <AppHeading
+                title="Secret Word"
+                subtitle="Você não pode dizer uma palavra secreta?"
+            
+            />
             <button type="button" className="btn">Inicie o Jogo</button>
-        </div>
+        </main>
     );
 };
 
