d600125c8c0648b7c2ec273f81c23d91c5cec406
Davidson Ratis <davidsonmakarios@gmail.com>
2022-09-20 16:42:32 -0300

    category, word and points data

---
+++ src/data/gameData.js
@@ -0,0 +1,26 @@
+export const wordsList = {
+    0: ["Suspensão", "Motor", "Capô", "Retrovisor", "Pistão"],
+    1: ["Ameixa", "Tamarindo", "Seriguela", "Mamão", "Laranja", "Kiwi", "Morango"],
+    2: ["Fígado", "Cérebro", "Intestino", "Tireoide", "Pulmões", "Côrneas"],
+    3: ["Notebook", "Tablet", "Smartphone", "Teclado", "Processador", "Monitor"],
+    4: ["Linguagem", "Framework", "Código", "Escopo", "Variáveis"],
+    5: ["Sushi", "Tapioca", "Feijoada", "Lasanha", "Cuscuz"],
+};
+
+export const categoryList = {
+    0: "Carro",
+    1: "Fruta",
+    2: "Corpo",
+    3: "Tecnologia",
+    4: "Programação",
+    5: "Comida",
+};
+
+export const categoryPoints = {
+    0: 20,
+    1: 28,
+    2: 45,
+    3: 35,
+    4: 60,
+    5: 30,
+};
