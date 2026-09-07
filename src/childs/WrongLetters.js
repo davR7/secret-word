c1215340988517868d8e1c81f252eb5691b652a7
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 18:26:31 -0300

    refactor: replace WrongWords component with WrongLetters

---
+++ src/childs/WrongLetters.js
@@ -0,0 +1,33 @@
+import './css/WrongLetters.css';
+import { useGame } from '../hooks/useGame';
+
+const WrongLetters = () => {
+    const { game } = useGame();
+
+    return (
+        <div className="wrong-letters">
+            <span className="wrong-letters__title">
+                Letras erradas:
+            </span>
+
+            <div className="wrong-letters__list">
+                {game.wrongLetters.length > 0 ? (
+                    game.wrongLetters.map((letter) => (
+                        <span
+                            className="wrong-letters__letter"
+                            key={letter}
+                        >
+                            {letter}
+                        </span>
+                    ))
+                ) : (
+                    <span className="wrong-letters__empty">
+                        Nenhuma ainda
+                    </span>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default WrongLetters;
