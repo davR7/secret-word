c8c956c0b238ea65434feab07ddd927e579a2808
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 17:23:37 -0300

    refactor: use MAX_REVEALED_LETTERS and MAX_GUESSES

--- src/contexts/GameContextProps.js
+++ src/contexts/GameContextProps.js
@@ -1,15 +1,17 @@
+import { MAX_GUESSES, MAX_REVEALED_LETTERS } from "../shared/constants";
+
 export const initialGameState = {
   onStage: "start",
   onPopUp: "",
   points: 0,
   score: 0,
-  square: 2,
+  revealedLetters: MAX_REVEALED_LETTERS,
   question: {
     word: "",
     clue: "",
   },
   letters: [],
-  guesses: 3,
+  guesses: MAX_GUESSES,
   guessedLetters: [],
   wrongLetters: [],
 };
