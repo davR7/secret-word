db2f849257aacd6fb7e976d433431c4d5efe9fa2
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 16:41:39 -0300

    refactor: update PopUp component structure

--- src/childs/PopUp.js
+++ src/childs/PopUp.js
@@ -1,25 +1,23 @@
 import "./css/PopUp.css"
+import Button from './Button';
 import { useGame } from "../hooks/useGame"
 
 const PopUp = ({title, text, textBtn, eventBtn}) => {
-    const { gameProps } = useGame()
-    const { onPopUp } = gameProps
+    const { game } = useGame()
+    const { onPopUp } = game
 
     const flex = text ? "" : "flex-row-c"
-    const bg = onPopUp === "victory" ? "pop-up__victory" : 
-        onPopUp === "defeat" ? "pop-up__defeat" : ""
-
+    const bg = onPopUp === "victory" 
+        ? "pop-up__victory" 
+        : onPopUp === "continue" ? "pop-up__continue" 
+        : onPopUp === "defeat" ? "pop-up__defeat" : ""
+    
     return (
-        <div className="pop-up flex-row-c">
-            <div 
-                className={`pop-up__inner ${bg} ${flex}`} 
-                style={onPopUp !== "defeat" ? {textAlign: "left"} : {textAlign: "center"}}
-                >
-                <button type="btn" className="pop-up__btn btn" onClick={eventBtn}>{textBtn}</button>
+        <div className="pop-up flex-row-xy">
+            <div className={`pop-up__inner ${bg} ${flex}`}>
                 <h3 className="pop-up__title">{title}</h3>
-                {text ? 
-                    (<p className="pop-up__text">{text}</p>) : 
-                    (<></>)}
+                <p className="pop-up__text">{text}</p>
+                <Button type="type" className="pop-up__btn" onClick={eventBtn}>{textBtn}</Button>
             </div>
         </div>
     )
