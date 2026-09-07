dcdb7bbd6fad21c17834a51afa06f93c579a3293
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 15:53:44 -0300

    feat: implement GameContextProps

---
+++ src/contexts/GameContextProps.js
@@ -0,0 +1,15 @@
+export const initialGameState = {
+  onStage: "start",
+  onPopUp: "",
+  points: 0,
+  score: 0,
+  square: 2,
+  question: {
+    word: "",
+    clue: "",
+  },
+  letters: [],
+  guesses: 3,
+  guessedLetters: [],
+  wrongLetters: [],
+};
