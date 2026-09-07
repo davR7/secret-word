838bda8266298bd34457b974cda89b8c486d75e1
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 13:05:26 -0300

    refactor: update the game data format

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,26 +1,23 @@
-export const wordsList = {
-    0: ["Suspensão", "Motor", "Capô", "Retrovisor", "Pistão"],
-    1: ["Ameixa", "Tamarindo", "Seriguela", "Mamão", "Laranja", "Kiwi", "Morango"],
-    2: ["Fígado", "Cérebro", "Intestino", "Tireoide", "Pulmão", "Côrnea"],
-    3: ["Notebook", "Tablet", "Smartphone", "Teclado", "Processador", "Monitor"],
-    4: ["Linguagem", "Framework", "Código", "Escopo", "Variáveis"],
-    5: ["Sushi", "Tapioca", "Feijoada", "Lasanha", "Cuscuz"],
-};
+import { biology } from "./questions/biology";
+import { car } from "./questions/car";
+import { food } from "./questions/food";
+import { technology } from "./questions/technology";
 
-export const categoryList = {
-    0: "Carro",
-    1: "Fruta",
-    2: "Corpo",
-    3: "Tecnologia",
-    4: "Programação",
-    5: "Comida",
-};
-
-export const categoryPoints = {
-    0: 20,
-    1: 28,
-    2: 45,
-    3: 35,
-    4: 60,
-    5: 30,
+export const questionList = {
+  car: {
+    points: 250,
+    questions: car,
+  },
+  biology: {
+    points: 200,
+    questions: biology,
+  },
+  food: {
+    points: 150,
+    questions: food,
+  },
+  technology: {
+    points: 300,
+    questions: technology,
+  },
 };
