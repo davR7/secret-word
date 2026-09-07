cd1e39b4c235945e3aa33ac79e772a219868535e
davidson ratis <davidsonmakarios@gmail.com>
2026-09-07 16:34:53 -0300

    refactor: update AppHeading component structure

--- src/childs/AppHeading.js
+++ src/childs/AppHeading.js
@@ -1,23 +1,17 @@
 import './css/AppHeading.css';
 
-const AppHeading = ({ title, subtitle, text }) => {
-    const arr = subtitle.split(": ")
-    
-        return(
-            <div className="heading">
-                <h1 className="heading__title">{title}</h1>
-                {arr.length === 2 ?
-                    (<h4 className="heading__subtitle">{`${arr[0]}: `}
-                        <span className="heading__highlighted-word"> {arr[1]}</span>
-                    </h4>) :
-                    (<h4 className="heading__subtitle">{arr[0]}</h4>)
-                }
-                {text ?
-                    (<p className="heading__text">{text}</p>) :
-                    (<></>)
-                }
+const AppHeading = ({ prefix, title, description }) => {
+    return(
+        <div className="heading">
+            <h1 className="title">
+                {prefix && (<span className="title__prefix">{prefix}</span>)}
+                <span className="title__main">{title}</span>
+            </h1>
+            <div className="heading__wrapper flex-row-xy">
+                <p className="heading__description">{description}</p>
             </div>
-        );
+        </div>
+    );
 };
 
 export default AppHeading;
